201d929666821506e0fcb30afbc71da43b6457a0
Benn Herrera <benn.herrera@pm.me>
2024-06-26 23:48:45 -0700

    basic doc more convenient version spec handling.

---
+++ README.md
@@ -0,0 +1,45 @@
+# vku
+## A single header C++ type library for Vulkan
+### Principles
+* Minimal coding mercy (no RAII)
+* Unopinionated
+    * Just adds some mercy utility types and simple, useful functions
+    * Does not constrain usage of Vulkan
+    * None of the Vulkan API functions are wrapped
+* No additional dependencies
+* No use of std:: strings or containers
+* No boost syndrome
+    * No bloat or major compilation overhead.
+    * Using one function or type does not compel the use of others
+    * All types are mix & match with base API - incremental adoption is painless
+
+### Types
+* Structs subclassed with default zero initialization
+    * sType assigned when present 
+* Enum wrappers with default initialization to invalid values
+* Flags type wrappers with default zero initialization
+* Handle type wrappers with default null initialization
+* VK_NULL_HANDLE definition added if not defined
+
+All wrapped types work interchangeably with base types in the base API
+
+### Metadata Functions
+* const char* vku::to_string(<VkEnumType>) added for all enum types
+* VK_FORMAT property getters and predicates
+* const char* vku::to_string(const VkStructType&, const void* member) for selected classes
+
+## Usage
+* For Existing Versions
+    * Clone repo and add [vk-sdk-version]/include to your project's include path
+    * all inline
+        * add -DVKU_INLINE_ALL to compiler options
+        * #include "vku/vku.h"
+    * compile utility functions
+       * in one C++ source file define VKU_IMPLEMENT before including vku/vku.h 
+* Adding a new version
+    * ./gen_vku.sh <MAJ.min.patch> # eg 1.3.280
+    * version must match a tagged SDK release in https://github.com/KhronosGroup/VulkanHeaders
+    * ./gen_vku.sh -h for more details.
+
+### TODO:
+* CMake file with install rule
--- gen_vku.sh
+++ gen_vku.sh
@@ -4,14 +4,17 @@ THIS_DIR=$(dirname "${0}")
 # can't rely on realpath existing
 THIS_DIR=$(cd "${THIS_DIR}"; pwd)
 
-DEFAULT_SDK_VER="1.3.283.0"
+DEFAULT_SDK_VER="1.3.283"
 VK_HEADERS_NAME="Vulkan-Headers"
 VK_HEADERS_REPO="https://github.com/KhronosGroup/${VK_HEADERS_NAME}.git"
 
 function usage() {
     echo "usage: ${THIS_SCRIPT} [sdk-version]"
-    echo "    where sdk-version is MAJ.min.patch.fix."
+    echo "    where sdk-version is MAJ.min[.patch][.fix] e.g. 1.3 or 1.3.283 or 1.3.283.0"
     echo "    version must match a tagged version in ${VK_HEADERS_REPO}"
+    echo "    when multiple tags match the most recent version is taken."
+    echo "    priority is given to sdk release tags (vulkan-sdk-M,m.p.f)"
+    echo "    if you need a newer version than the last sdk tagged version use the full M.m.p.f version"
     echo "    default is ${DEFAULT_SDK_VER}"
     exit ${1}
 }
@@ -24,7 +27,6 @@ case "${1}" in
 esac
 
 VK_SDK_VER=${1:-${DEFAULT_SDK_VER}}
-VK_HEADERS_TAG="vulkan-sdk-${VK_SDK_VER}"
 
 if ! PYTHON=$(which python3 2> /dev/null); then
   if ! PYTHON=$(which python 2> /dev/null); then
@@ -44,17 +46,25 @@ fi
 GEN_VKU_PY="${THIS_DIR}/gen_vku.py"
 VK_HEADERS_SANDBOX="${THIS_DIR}/${VK_HEADERS_NAME}"
 VK_XML="${THIS_DIR}/${VK_HEADERS_NAME}/registry/vk.xml"
-VKU_H="${THIS_DIR}/${VK_SDK_VER}/vku.h"
 
 set -eux
 
 if [[ ! -d "${VK_HEADERS_SANDBOX}" ]]; then
   git clone "${VK_HEADERS_REPO}" "${VK_HEADERS_SANDBOX}"
 fi
-(
-  cd "${VK_HEADERS_SANDBOX}" &&
-    git fetch  &&
-    git -c advice.detachedHead=false checkout "${VK_HEADERS_TAG}"
-)
+
+(cd "${VK_HEADERS_SANDBOX}" && git fetch --tags)
+VK_HEADERS_TAG=$(cd "${VK_HEADERS_SANDBOX}" && git tag | grep -e "${VK_SDK_VER//\./\\.}" | sort -Vr | head -1)
+(cd "${VK_HEADERS_SANDBOX}" && git -c advice.detachedHead=false checkout "${VK_HEADERS_TAG}")
+
 test -r "${VK_XML}" || (echo "${VK_XML} missing." 1>&2 && false)
+# normalize tags (vulkan-sdk-M.m.p.f and vM.m.p become vM.m.p)
+# just the digits and dots
+VK_VERSION="${VK_HEADERS_TAG//[^0-9.]/}"
+# prepend v and strip the fix number. most recent fix version should always prevail.
+VK_VERSION=$(set ${VK_VERSION//\./ }; echo "v${1}.${2}.${3}")
+VKU_H="${THIS_DIR}/${VK_VERSION}/include/vku/vku.h"
+
 "${PYTHON}" "${GEN_VKU_PY}" "${VK_XML}" "${VKU_H}"
+git add "${VKU_H}"
+git commit -m "generated vku.h"
